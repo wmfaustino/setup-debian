0b81c5626860c779bbd871f7a030eb39a9fdfe71
wmfaustino <wfaustino.dev@gmail.com>
2020-08-19 16:50:25 -0300

    removing sudo and add y to apt install

--- install/virtualbox.in.sh
+++ install/virtualbox.in.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-echo deb '[arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib' | sudo tee --append '/etc/apt/sources.list.d/virtualbox.list'
+echo deb '[arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib' | tee --append '/etc/apt/sources.list.d/virtualbox.list'
 
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
 
-sudo apt-get update
-sudo apt-get install virtualbox-6.1
+apt-get update -y
+apt-get install virtualbox-6.1 -y
 
-sudo apt-get install virtualbox-6.1
+apt-get install virtualbox-6.1 -y
 
