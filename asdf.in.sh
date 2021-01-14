16136e83c1e65b53030d80d98d7339aecbebb35d
wmfaustino <wfaustino.dev@gmail.com>
2021-01-14 19:11:09 -0300

    languages setup functions

--- asdf.in.sh
+++ asdf.in.sh
@@ -11,24 +11,24 @@
 ###=== VARIABLES ===
 
 # --- asdf default dir
-: ${ASDF_DATA_DIR:='$HOME/.asdf'}
+ASDF_DATA_DIR="$HOME/.asdf"
 
 # --- bash
-_bashrc="${HOME}/.bashrc"
+declare -rg _bashrc="${HOME}/.bashrc"
 
-declare -arg bashrc_files_to_source=(
-  .\ \""${ASDF_DATA_DIR}/asdf.sh"\"
-  .\ \""${ASDF_DATA_DIR}/completions/asdf.bash"\"
+declare -arg source_into_bashrc=(
+  .\ \"'${ASDF_DATA_DIR}'/asdf.sh\"
+  .\ \"'${ASDF_DATA_DIR}'/completions/asdf.bash\"
 )
 
 # exec para pegar a ZDOTDIR
 # --- zsh
-  _zshrc="${ZDOTDIR:=$HOME/.config/zsh}/.zshrc"
+declare -rg _zshrc="${ZDOTDIR:=$HOME/.config/zsh}/.zshrc"
 
-declare -arg  zshrc_files_to_source=(
-    .\ \""${ASDF_DATA_DIR}/asdf.sh"\"
+declare -arg source_into_zshrc=(
+    .\ \"'${ASDF_DATA_DIR}'/asdf.sh\"
     '# append completions to fpath'
-    "fpath=(${ASDF_DATA_DIR}/completions $fpath)"
+    "fpath=(${ASDF_DATA_DIR}/completions \$fpath)"
     '# initialise completions with ZSH''s compinit'
     'autoload -Uz compinit'
     'compinit'
@@ -57,13 +57,10 @@ function _setup_config_file(){
   return "$?"
 }
 
- # _setup_config_file "$_bashrc" "${bashrc_files_to_source[@]}"
-# _setup_config_file "$_zshrc" "${zshrc_files_to_source[@]}"
+_setup_config_file "$_bashrc" "${source_into_bashrc[@]}"
+_setup_config_file "$_zshrc" "${source_into_zshrc[@]}"
 
 echo $ASDF_DATA_DIR
-
-exit
-
 
 
 # Plugins
@@ -72,51 +69,67 @@ exit
 _in_asdf_nodejs(){
   # https://github.com/asdf-vm/asdf-nodejs
 
-  local -r dependencies="dirmngr gpg curl"
-  local -r asdf_nodejs="https://github.com/asdf-vm/asdf-nodejs.git"
+  sudo apt install dirmngr gpg curl -y
 
-  sudo apt install "$dependencies"
-
-  asdf plugin-add nodejs "$asdf_nodejs"
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
   bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 
+  return "$?"
+}
+
+_in_asdf_golang(){
+  # https://github.com/kennyp/asdf-golang
+
+  sudo apt install coreutils curl -y
+
+  asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+
+  # After using go get to install a package you need to run asdf reshim golang to get any new shims.
+
+  return "$?"
+}
+
+_in_asdf_rust(){
+  # https://github.com/code-lever/asdf-rust
+
+  asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+
+# After you have installed rust, do NOT follow the directions it outputs to update your PATH -- asdf's shim will handle that for you!
+
+  return "$?"
+}
+
+_in_asdf_ruby(){
+  # https://github.com/asdf-vm/asdf-ruby
+
+  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+  return "$?"
+}
+
+_in_asdf_python(){
+  # https://github.com/danhper/asdf-python
+
+  # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+  sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+  asdf plugin-add python
+
+# If you use pip to install a module like ipython that has binaries. You will need to run asdf reshim python for the binary to be in your path.
+
+  return "$?"
+}
+
+_in_asdf_lua(){
+  # https://github.com/Stratus3D/asdf-lua
+
+  sudo apt-get install linux-headers-$(uname -r) build-essential -y
+
+  asdf plugin-add lua https://github.com/Stratus3D/asdf-lua.git
+
+  return "$?"
 }
 
 
 
-go
-
-https://github.com/kennyp/asdf-golang
-
-apt install coreutils
-apt install curl
-
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-
-# After using go get to install a package you need to run asdf reshim golang to get any new shims.
-
-rust
-
-https://github.com/code-lever/asdf-rust
-
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-
-# After you have installed rust, do NOT follow the directions it outputs to update your PATH -- asdf's shim will handle that for you!
-
-ruby
-https://github.com/asdf-vm/asdf-ruby
-
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-python
-
-https://github.com/danhper/asdf-python
-
-asdf plugin-add python
-
-# If you use pip to install a module like ipython that has binaries. You will need to run asdf reshim python for the binary to be in your path.
-
-https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-
-sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
