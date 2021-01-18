87a8754009a3ac64101f853d4859afdc3f235e8f
wmfaustino <wfaustino.dev@gmail.com>
2021-01-17 23:52:14 -0300

    adding and installing plugins functions working

--- asdf.in.sh
+++ asdf.in.sh
@@ -87,16 +87,6 @@ declare -Ag _setup_shell=(
   [zshrc]=0
 )
 
-# --- add plugins
-declare -Ag _add_plugin=(
-  [golang]=0
-  [lua]=0
-  [neovim]=0
-  [nodejs]=0
-  [ruby]=0
-  [rust]=0
-  [python]=0
-)
 # =========================================================
 
 # --- asdf default dir
@@ -126,50 +116,67 @@ declare -arg source_into_zshrc=(
 # === ASDF PLUGINS ===
 # https://asdf-vm.com/#/plugins-all
 
+# --- add plugins
+declare -arg plugins=(
+  golang
+  lua
+  neovim
+  nodejs
+  ruby
+  rust
+  python
+)
+
 # --- golang
 # After using go get to install a package you need to run asdf reshim golang to get any new shims.
-declare -Arg golang=(
+declare -Ag golang=(
   [dependencies]="coreutils curl"
   [repo]="https://github.com/kennyp/asdf-golang.git"
+  [add]=0
   [version]="latest"
 )
 # =========================================================
 
 # --- lua
-declare -Arg lua=(
+declare -Ag lua=(
   [dependencies]="linux-headers-$(uname -r) build-essential"
   [repo]="https://github.com/Stratus3D/asdf-lua.git"
+  [add]=0
   [version]="latest"
 )
 # =========================================================
 
 # --- neovim
 # https://github.com/richin13/asdf-neovim
-declare -Arg neovim=(
+declare -Ag neovim=(
+  [add]=0
   [version]="nightly"
 )
 # =========================================================
 
 # --- nodejs
-declare -Arg nodejs=(
+declare -Ag nodejs=(
   [dependencies]="dirmngr gpg curl"
   [repo]="https://github.com/asdf-vm/asdf-nodejs.git"
   [keys]='${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+  [add]=0
   [version]="latest"
   )
 # =========================================================
 
 # --- ruby
-declare -Arg ruby=(
+declare -Ag ruby=(
   [repo]="https://github.com/asdf-vm/asdf-ruby.git"
+  [add]=0
   [version]="latest"
 )
 # =========================================================
 
 # --- rust
 # After you have installed rust, do NOT follow the directions it outputs to update your PATH -- asdf's shim will handle that for you!
-declare -Arg rust=(
+declare -Ag rust=(
   [repo]="https://github.com/code-lever/asdf-rust.git"
+  [add]=0
   [version]="latest"
 )
 # =========================================================
@@ -178,8 +185,9 @@ declare -Arg rust=(
 # https://github.com/danhper/asdf-python
 # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
 # If you use pip to install a module like ipython that has binaries. You will need to run asdf reshim python for the binary to be in your path.
-declare -Arg python=(
+declare -Ag python=(
   [dependencies]="make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
+  [add]=0
   [version]="latest"
 )
 # =========================================================
@@ -218,30 +226,22 @@ function _add_asdf_plugin(){
   local -n plugin="$1"
 
   # Install plugin dependencies
-  sudo apt install -qq -y ${plugin[dependencies]} > /dev/null 2>&1
+  if [[ ! -z "${plugin[dependencies]}" ]]; then
+
+    echo "Plugin $1 requires ${plugin[dependencies]}"
+    sudo apt install -qq -y ${plugin[dependencies]} > /dev/null 2>&1
+  fi
 
   # first remove the plugin if it is already added
   # it removes first to avoid any gpg keys issue
+  echo "Adding $1 plugin to asdf"
   asdf plugin remove "$1"
   asdf plugin add "$1" "${plugin[repo]}"
 
   # Add gpg keys (if plugin requires)
   [[ ! -z "${plugin[keys]}" ]] && bash -c "${plugin[keys]}"
 
-
-  return "$0"
-}
-
-function _in_asdf_plugin(){
-
-  # $1 is the plugin associative array (dependencies, repo, key(gpg)
-  # plugin will reference that associative array
-  local -n plugin="$1"
-
-  # After adding a plugin to asdf, you can install any version available
-  # The version that will be installed is especified at each plugin associative array
-  (( "${_add_plugin[plugin_latest]}" == 1 )) && asdf install "$1" "${plugin[version]}"
-
+  return 0
 }
 
 # =========================================================
@@ -266,13 +266,21 @@ _main(){
 
   done
 
-  # --- Install Plugins
-  for plugin in "${!_add_plugin[@]}"; do
+  # --- Adding Plugins
+  for plugin in "${plugins[@]}"; do
 
-    (( "${_add_plugin[$plugin]}" == 0 )) && \
-    _add_asdf_plugin "$plugin"
+    local -n plugin_arr="$plugin"
 
+    (( "${plugin_arr[add]}" == 1 )) && {
+
+    _add_asdf_plugin "$plugin";
+
+    (( "${_install[plugin_latest]}" == 1 )) && asdf install "$plugin" "${plugin_arr[version]}"
+
+  }
   done
+
+  # --- Instaling a plugin version
 
   exit 0
 }
@@ -288,19 +296,24 @@ while [ -n "${1}" ]; do
             "--all-shells")
                         "${_setup_shell[bashrc]}"=1
                         "${_setup_shell[zshrc]}"=1        ;;
-            "--golang"    ) "${_add_plugin[golang]}"=1    ;;
-            "--lua"       ) "${_add_plugin[lua]}"=1       ;;
-            "--neovim"    ) "${_add_plugin[neovim]}"=1    ;;
-            "--nodejs"    ) "${_add_plugin[nodejs]}"=1    ;;
-            "--ruby"      ) "${_add_plugin[ruby]}"=1      ;;
-            "--rust"      ) "${_add_plugin[rust]}"=1      ;;
-            "--python"    ) "${_add_plugin[python]}"=1    ;;
-            "--latest"    ) "${_install[plugin_latest]}"=1;;
+            "--golang" | \
+            "--lua"    | \
+            "--neovim" | \
+            "--nodejs" | \
+            "--ruby"   | \
+            "--rust"   | \
+            "--python"    )
+                        declare -n plugin_arr="${1#--*}";
+                        plugin_arr[add]=1;
+                        ;;
             "--add-all"   )
-                           for plugin in "${!_add_plugin[@]}";
-                             do
-                               _add_plugin[$plugin]=1
-                             done                         ;;
+                        for plugin in "${plugins[@]}";
+                          do
+                            declare -n plugin_arr="$plugin"
+                            plugin_arr[add]=1
+                          done
+                        ;;
+            "--latest"    ) _install[plugin_latest]=1;;
             "--help"      ) _usage; exit 0                ;;
             *             )
                         echo "Invalid option. ${1}"
